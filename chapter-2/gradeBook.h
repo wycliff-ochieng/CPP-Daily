d80bb0e2b45c27cb5670b4ba4a79cfbf914dfe89
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-08-12 12:02:57 +0300

    classes/interfaces

---
+++ chapter-2/gradeBook.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+class GradeBook
+{
+    public:
+
+    explicit GradeBook(std::string name)
+        :   courseName(name)
+    {
+
+    }
+    void setCourseName(std::string name)
+    {
+        courseName = name;
+    }
+    std::string getCourseName() const
+    {
+        return courseName;
+    }
+    void displayMessage() const
+    {
+        std::cout << "Message " << courseName << std::endl;
+    }
+
+    private:
+    std::string courseName;
+};
